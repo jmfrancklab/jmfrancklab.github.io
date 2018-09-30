4d09067dd0da06c3a00aa3486ffee3c8a5c7e49d
John Franck (laptop) <jmfranck@syr.edu>
2018-09-30 16:55:20 -0400

    researched showed up twice

--- apply_citations.sh
+++ apply_citations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-pandoc AAResearch_citations.md -f markdown -t markdown-citations --csl=edited-pmid-format.csl --bibliography library_abbrev_utf8.bib -o AAResearch.md
+pandoc citeproc_src/AAResearch_citations.md -f markdown -t markdown-citations --csl=edited-pmid-format.csl --bibliography library_abbrev_utf8.bib -o AAResearch.md
 # add the header, which gets omitted
-{ head -5 AAResearch_citations.md; cat AAResearch.md; } > AAResearch.md.new
+{ head -5 citeproc_src/AAResearch_citations.md; cat AAResearch.md; } > AAResearch.md.new
 mv AAResearch.md.new AAResearch.md
 sed -i 's/\^\([0-9,\\-]\+\)\^/<sup>\1<\/sup>/g' AAResearch.md
 sed -i 's/~\([^~]\{0,10\}\)~/<sub>\1<\/sub>/g' AAResearch.md
